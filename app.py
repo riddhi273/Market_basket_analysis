b666fdec13102f7015fd8902015fde2f8df930e3
riddhi273 <riddhimakani273@gmail.com>
2026-04-06 20:17:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -178,8 +178,8 @@ if data_option == "Upload New Dataset":
 
     else:
     
-        dataset = pd.read_csv("./dataset_small.csv", encoding="latin1")
-        dataset = prepare_dataset(dataset)
+            dataset = pd.read_csv("./dataset_small.csv", encoding="latin1")
+            dataset = prepare_dataset(dataset)
 
     rfm = generate_rfm(dataset)
     best_rules = generate_rules(dataset)
